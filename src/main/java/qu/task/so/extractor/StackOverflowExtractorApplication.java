87fbe0d11230ff1eb7f497dcc2f120da7f35f9e1
Hamed Mirzaei <mirzaeihamed1368@gmail.com>
2019-12-16 21:25:04 +0330

    first commit

---
+++ src/main/java/qu/task/so/extractor/StackOverflowExtractorApplication.java
@@ -0,0 +1,13 @@
+package qu.task.so.extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverflowExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackOverflowExtractorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/qu/task/so/extractor/StackOverflowExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package qu.task.so.extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackOverflowExtractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
